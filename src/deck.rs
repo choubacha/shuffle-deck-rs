9b0f6b4014946e3fb00b68cd57c57f0951de410d
Kevin Choubacha <chewbacha@gmail.com>
2018-09-13 11:10:59 -0700

    Add some display elements in and extract deck to separate module

---
+++ src/deck.rs
@@ -0,0 +1,238 @@
+use rand::Rng;
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum Suit {
+    Club,
+    Spade,
+    Heart,
+    Diamond,
+}
+
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let c = match self {
+            Suit::Club => "C",
+            Suit::Spade => "S",
+            Suit::Heart => "H",
+            Suit::Diamond => "D",
+        };
+        write!(f, "{}", c)
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum Face {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+impl fmt::Display for Face {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let c = match self {
+            Face::Ace => "A",
+            Face::Two => "2",
+            Face::Three => "3",
+            Face::Four => "4",
+            Face::Five => "5",
+            Face::Six => "6",
+            Face::Seven => "7",
+            Face::Eight => "8",
+            Face::Nine => "9",
+            Face::Ten => "T",
+            Face::Jack => "J",
+            Face::Queen => "Q",
+            Face::King => "K",
+        };
+        write!(f, "{}", c)
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+struct Card(Suit, Face);
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.0, self.1)
+    }
+}
+
+const ALL_CARDS: [Card; 52] = [
+    Card(Suit::Club, Face::Ace),
+    Card(Suit::Club, Face::Two),
+    Card(Suit::Club, Face::Three),
+    Card(Suit::Club, Face::Four),
+    Card(Suit::Club, Face::Five),
+    Card(Suit::Club, Face::Six),
+    Card(Suit::Club, Face::Seven),
+    Card(Suit::Club, Face::Eight),
+    Card(Suit::Club, Face::Nine),
+    Card(Suit::Club, Face::Ten),
+    Card(Suit::Club, Face::Jack),
+    Card(Suit::Club, Face::Queen),
+    Card(Suit::Club, Face::King),
+    Card(Suit::Spade, Face::Ace),
+    Card(Suit::Spade, Face::Two),
+    Card(Suit::Spade, Face::Three),
+    Card(Suit::Spade, Face::Four),
+    Card(Suit::Spade, Face::Five),
+    Card(Suit::Spade, Face::Six),
+    Card(Suit::Spade, Face::Seven),
+    Card(Suit::Spade, Face::Eight),
+    Card(Suit::Spade, Face::Nine),
+    Card(Suit::Spade, Face::Ten),
+    Card(Suit::Spade, Face::Jack),
+    Card(Suit::Spade, Face::Queen),
+    Card(Suit::Spade, Face::King),
+    Card(Suit::Heart, Face::Ace),
+    Card(Suit::Heart, Face::Two),
+    Card(Suit::Heart, Face::Three),
+    Card(Suit::Heart, Face::Four),
+    Card(Suit::Heart, Face::Five),
+    Card(Suit::Heart, Face::Six),
+    Card(Suit::Heart, Face::Seven),
+    Card(Suit::Heart, Face::Eight),
+    Card(Suit::Heart, Face::Nine),
+    Card(Suit::Heart, Face::Ten),
+    Card(Suit::Heart, Face::Jack),
+    Card(Suit::Heart, Face::Queen),
+    Card(Suit::Heart, Face::King),
+    Card(Suit::Diamond, Face::Ace),
+    Card(Suit::Diamond, Face::Two),
+    Card(Suit::Diamond, Face::Three),
+    Card(Suit::Diamond, Face::Four),
+    Card(Suit::Diamond, Face::Five),
+    Card(Suit::Diamond, Face::Six),
+    Card(Suit::Diamond, Face::Seven),
+    Card(Suit::Diamond, Face::Eight),
+    Card(Suit::Diamond, Face::Nine),
+    Card(Suit::Diamond, Face::Ten),
+    Card(Suit::Diamond, Face::Jack),
+    Card(Suit::Diamond, Face::Queen),
+    Card(Suit::Diamond, Face::King),
+];
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Deck {
+    cards: Vec<Card>,
+}
+
+impl Deck {
+    pub fn deal(num_cards: usize) -> Deck {
+        let num_cards = if num_cards > 52 { 52 } else { num_cards };
+        Deck {
+            cards: Vec::from(&ALL_CARDS[0..num_cards]),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.cards.len()
+    }
+}
+
+impl fmt::Display for Deck {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let cards = self.cards
+            .iter().map(|c| { format!("{}", c) })
+            .collect::<Vec<String>>()
+            .as_slice()
+            .join(", ");
+        write!(f, "[{}]", &cards)
+    }
+}
+
+#[cfg(test)]
+mod deck_tests {
+    use super::*;
+
+    #[test]
+    fn decks_are_equal() {
+        assert_eq!(Deck::deal(12), Deck::deal(12));
+        assert_ne!(Deck::deal(13), Deck::deal(12));
+    }
+
+    #[test]
+    fn decks_can_be_displayed() {
+        assert_eq!(format!("{}", Deck::deal(4)), "[CA, C2, C3, C4]")
+    }
+}
+
+pub struct Shuffler<R: Rng> {
+    original: Deck,
+    shuffle: Deck,
+    rng: R,
+    shuffles: u128,
+}
+
+impl<R: Rng> Shuffler<R> {
+    pub fn new(deck: Deck, rng: R) -> Shuffler<R> {
+        Shuffler {
+            shuffle: deck.clone(),
+            original: deck,
+            rng,
+            shuffles: 0,
+        }
+    }
+
+    pub fn shuffle(&mut self) -> u128 {
+        self.rng.shuffle(&mut self.shuffle.cards);
+        self.shuffles += 1;
+        self.shuffles
+    }
+
+    pub fn deck_matches_original(&self) -> bool {
+        self.shuffle == self.original
+    }
+
+    pub fn shuffles(&self) -> u128 {
+        self.shuffles
+    }
+
+    pub fn len(&self) -> usize {
+        self.original.len()
+    }
+}
+
+impl<R: Rng> fmt::Display for Shuffler<R> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "cards:        {}", self.original)?;
+        writeln!(f, "last shuffle: {}", self.shuffle)?;
+        writeln!(f, "shuffles:     {}", self.shuffles)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod shuffler_tests {
+    use rand::thread_rng;
+    use super::*;
+
+    #[test]
+    fn displays_self() {
+        let shuffler = Shuffler::new(Deck::deal(3), thread_rng());
+        assert_eq!(
+            format!("{}", shuffler),
+            "cards:        [CA, C2, C3]\n\
+            last shuffle: [CA, C2, C3]\n\
+            shuffles:     0\n");
+    }
+
+    #[test]
+    fn knows_about_whether_we_match() {
+        let shuffler = Shuffler::new(Deck::deal(52), thread_rng());
+        assert!(shuffler.deck_matches_original());
+        shuffler.shuffle(); // The odds of this matching are astronomical.
+        assert!(!shuffler.deck_matches_original());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,151 +2,10 @@ extern crate clap;
 extern crate rand;
 
 use clap::{App, Arg};
-use rand::{thread_rng, Rng};
+use rand::thread_rng;
 use std::time::Instant;
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-enum Suit {
-    Club,
-    Spade,
-    Heart,
-    Diamond,
-}
-
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-enum Face {
-    Ace,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-}
-
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-struct Card(Face, Suit);
-
-const ALL_CARDS: [Card; 52] = [
-    Card(Face::Ace, Suit::Club),
-    Card(Face::Two, Suit::Club),
-    Card(Face::Three, Suit::Club),
-    Card(Face::Four, Suit::Club),
-    Card(Face::Five, Suit::Club),
-    Card(Face::Six, Suit::Club),
-    Card(Face::Seven, Suit::Club),
-    Card(Face::Eight, Suit::Club),
-    Card(Face::Nine, Suit::Club),
-    Card(Face::Ten, Suit::Club),
-    Card(Face::Jack, Suit::Club),
-    Card(Face::Queen, Suit::Club),
-    Card(Face::King, Suit::Club),
-    Card(Face::Ace, Suit::Spade),
-    Card(Face::Two, Suit::Spade),
-    Card(Face::Three, Suit::Spade),
-    Card(Face::Four, Suit::Spade),
-    Card(Face::Five, Suit::Spade),
-    Card(Face::Six, Suit::Spade),
-    Card(Face::Seven, Suit::Spade),
-    Card(Face::Eight, Suit::Spade),
-    Card(Face::Nine, Suit::Spade),
-    Card(Face::Ten, Suit::Spade),
-    Card(Face::Jack, Suit::Spade),
-    Card(Face::Queen, Suit::Spade),
-    Card(Face::King, Suit::Spade),
-    Card(Face::Ace, Suit::Heart),
-    Card(Face::Two, Suit::Heart),
-    Card(Face::Three, Suit::Heart),
-    Card(Face::Four, Suit::Heart),
-    Card(Face::Five, Suit::Heart),
-    Card(Face::Six, Suit::Heart),
-    Card(Face::Seven, Suit::Heart),
-    Card(Face::Eight, Suit::Heart),
-    Card(Face::Nine, Suit::Heart),
-    Card(Face::Ten, Suit::Heart),
-    Card(Face::Jack, Suit::Heart),
-    Card(Face::Queen, Suit::Heart),
-    Card(Face::King, Suit::Heart),
-    Card(Face::Ace, Suit::Diamond),
-    Card(Face::Two, Suit::Diamond),
-    Card(Face::Three, Suit::Diamond),
-    Card(Face::Four, Suit::Diamond),
-    Card(Face::Five, Suit::Diamond),
-    Card(Face::Six, Suit::Diamond),
-    Card(Face::Seven, Suit::Diamond),
-    Card(Face::Eight, Suit::Diamond),
-    Card(Face::Nine, Suit::Diamond),
-    Card(Face::Ten, Suit::Diamond),
-    Card(Face::Jack, Suit::Diamond),
-    Card(Face::Queen, Suit::Diamond),
-    Card(Face::King, Suit::Diamond),
-];
-
-#[derive(Debug, Eq, PartialEq, Clone)]
-struct Deck {
-    cards: Vec<Card>,
-}
-
-impl Deck {
-    fn deal(num_cards: usize) -> Deck {
-        let num_cards = if num_cards > 52 { 52 } else { num_cards };
-        Deck {
-            cards: Vec::from(&ALL_CARDS[0..num_cards]),
-        }
-    }
-
-    fn len(&self) -> usize {
-        self.cards.len()
-    }
-}
-
-#[cfg(test)]
-mod cause_i_like_tests {
-    use super::*;
-
-    #[test]
-    fn decks_are_equal() {
-        assert_eq!(Deck::deal(12), Deck::deal(12));
-        assert_ne!(Deck::deal(13), Deck::deal(12));
-    }
-}
-
-struct Shuffler<R: Rng> {
-    primary: Deck,
-    shuffle: Deck,
-    rng: R,
-    shuffles: u128,
-}
-
-impl<R: Rng> Shuffler<R> {
-    fn new(deck: Deck, rng: R) -> Shuffler<R> {
-        Shuffler {
-            shuffle: deck.clone(),
-            primary: deck,
-            rng,
-            shuffles: 0,
-        }
-    }
-
-    fn shuffle(&mut self) {
-        self.rng.shuffle(&mut self.shuffle.cards);
-        self.shuffles += 1;
-    }
-
-    fn deck_matches_primary(&self) -> bool {
-        self.shuffle == self.primary
-    }
-
-    fn len(&self) -> usize {
-        self.primary.len()
-    }
-}
+mod deck;
 
 fn main() {
     let matches = App::new("Deck shuffling magic")
@@ -166,27 +25,27 @@ fn main() {
         .unwrap_or("13")
         .parse()
         .expect("Deck size must be a number");
-    let deck = Deck::deal(deck_size);
+    let deck = deck::Deck::deal(deck_size);
 
-    let mut shuffler = Shuffler::new(deck, thread_rng());
-
-    assert!(shuffler.deck_matches_primary());
+    let mut shuffler = deck::Shuffler::new(deck, thread_rng());
 
     println!("Beginning to shuffle {} cards", shuffler.len());
 
     let start = Instant::now();
 
     loop {
-        shuffler.shuffle();
+        let shuffles = shuffler.shuffle();
 
-        if shuffler.deck_matches_primary() {
-            println!("We did it! It only took {} shuffles", shuffler.shuffles);
+        if shuffler.deck_matches_original() {
+            println!("We did it! It only took {} shuffles", shuffles);
             break;
-        } else if shuffler.shuffles % 10_000_000 == 0 {
+        } else if shuffler.shuffles() % 10_000_000 == 0 {
             println!(
-                "Still shuffling after {} shuffles, ~{} shuffles/sec",
-                shuffler.shuffles,
-                shuffler.shuffles / start.elapsed().as_secs() as u128
+                "Still shuffling...\n\
+                at ~{} shuffles/sec\n\
+                {}",
+                shuffles / start.elapsed().as_secs() as u128,
+                shuffler,
             );
         }
     }
